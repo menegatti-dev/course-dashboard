d5ae4350dc8aa5e101ae5690bff37a48b3c59fdb
Marlon Menegatti <devmenegatti@gmail.com>
2020-03-26 19:39:31 -0300

    Definicação de estilos globais da aplicação, zerando propriedades de Padding, Margin, Outline e definindo configurações de fonte padrão, background cursor e box-sizing.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {
+  BrowserRouter
+} from "react-router-dom";
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <h1>Course Dashboard</h1>
+    <BrowserRouter>
+      <GlobalStyle />
+    </BrowserRouter>
   );
 }
 
---
+++ src/styles/global.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #DFE1EB;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px Roboto, sans-serif;
+  }
+
+  #root {
+    cursor: default;
+  }
+`;
